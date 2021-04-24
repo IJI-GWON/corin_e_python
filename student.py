146cd6ee5a66b46fbe2e7f2d380e5d18a3c0d81f
IJI-GWON <81689507+IJI-GWON@users.noreply.github.com>
2021-04-24 14:32:48 +0900

    Add files via upload

---
+++ student.py
@@ -0,0 +1,17 @@
+﻿import requests
+import os
+
+sessioin = requests.Session()
+
+for i in range(2016, 2017):
+    if not os.path.isdir(str(i)):
+        os.makedirs(str(i))
+
+    for j in range(10000, 13500):
+        studentnumber = str(i) + str(j).zfill(5)
+        response = sessioin.get("http://kutis.kyonggi.ac.kr/webkutis/TransferImageStream.do?hakbun=" + studentnumber)
+
+        if response.content:
+            f = open(str(i) + "/" + studentnumber + ".jpg", 'wb')
+            f.write(response.content)
+            f.close()
